d14841b7f9621acb644461a6ad357235071ca6c9
jawad Bin Azam <abidibnazam@gmail.com>
2023-08-03 19:25:36 +0600

    setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Footer, Header, Todo, Welcome } from "./components";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1>Hello World</h1>
+      <div>
+        <Header></Header>
+        <Welcome></Welcome>
+      </div>
+      <Todo></Todo>
+      <Footer></Footer>
     </>
   );
 }
